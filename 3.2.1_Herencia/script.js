50aa6dee062587e7f9fd77ab8f1543fc9aac7d47
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-07-02 21:35:14 -0600

    Se agregan ejemplos de typeof

--- 3.2.1_Herencia/script.js
+++ 3.2.1_Herencia/script.js
@@ -1,10 +1,11 @@
 //1. Escribir la palabra reservara class seguida del nombre de la clase
 class Pokemon{
     // 2. Definir los atributos, esto se hace dentro del constructor.
-    constructor(nombre, nivelAtaque, nivelDeVida){
+    constructor(nombre, nivelAtaque, nivelDeVida, tipo='Básico'){
         this.nombre = nombre;
         this.nivelAtaque = nivelAtaque;
         this.nivelDeVida = nivelDeVida;
+        this.tipo = tipo;
     }
 
     // 3. Definir los métodos de la clase.
@@ -27,7 +28,7 @@ class PokemonFuego extends Pokemon{
     constructor(nombre, nivelAtaque, nivelDeVida, colorDeFlama){
         // La clase hija debe mandar a llamar al constructor de la clase padre.
         // Esto se hace con el método super() que debe recibir los parámetros del constructor de la clase padre.
-        super(nombre, nivelAtaque, nivelDeVida);
+        super(nombre, nivelAtaque, nivelDeVida, 'Fuego');
         this.colorDeFlama = colorDeFlama;
     }
 
@@ -41,6 +42,24 @@ class PokemonFuego extends Pokemon{
     }
 }
 
+class PokemonElectricidad extends Pokemon{
+    constructor(nombre, nivelAtaque, nivelDeVida, colorDeRayo){
+        // La clase hija debe mandar a llamar al constructor de la clase padre.
+        // Esto se hace con el método super() que debe recibir los parámetros del constructor de la clase padre.
+        super(nombre, nivelAtaque, nivelDeVida, 'Electricidad');
+        this.colorDeRayo = colorDeRayo;
+    }
+
+    electrocutar(pokemon){
+        console.log('--------COMENZANDO ATAQUE--------');
+        console.log(`El ${this.nombre} está atacando a ${pokemon.nombre}`);
+        console.log(`El nivel de vida de ${pokemon.nombre} ANTES del ataque es: ${pokemon.nivelDeVida}`);
+        console.log(`Electrocutando con nivel de ataque ${this.nivelAtaque}...`);
+        pokemon.nivelDeVida -= this.nivelAtaque * 1.8;
+        console.log(`El nivel de vida de ${pokemon.nombre} DESPUÉS del ataque es: ${pokemon.nivelDeVida}`);
+    }
+}
+
 let pokemon1 = new Pokemon('Pikachu', 15, 200);
 let pokemon2 = new PokemonFuego('Charmander', 10, 300, 'Azul');
 
@@ -50,9 +69,31 @@ pokemon2.atacar(pokemon1);
 pokemon1.atacar(pokemon2);
 pokemon2.lanzarFuego(pokemon1);
 
+let numero = 10;
+let pokemonLiteral = {
+    nombre: 'Raichu',
+    nivelDeAtaque: 25
+}
+
+console.log(typeof numero);
+console.log(typeof pokemon1);
+console.log(typeof pokemonLiteral);
+
+if(typeof pokemon2 == 'object'){
+    if(pokemon2.tipo == 'Fuego'){
+        console.log('Tipo fuego');
+    }else{
+        console.log('Tipo básico');
+    }
+}else{
+    console.log('Lo que estás evaluando no es un objeto, por lo tanto, no tiene propiedades');
+}
+
+
 // Ejercicio
 // 1. Crear otros 2 tipos de Pokemon
 // 2. Agregar un atributo y método específico a cada tipo de pokemon creado.
 // 3. Van a crear una función que reciba como parámetros a 2 pokemones. 
 //      Esta función debe detectar automáticamente el tipo de Pokemon que recibe como parámetro y atacar con su ataque especial.
 //      Pista: Investigar la palabra reservada typeof
+//      Pista: alternativa: instanceof
